21c5b7c3efed5a6fdc60bffe7440894b195b30ab
Amir Shabani <amir@shabani.dev>
2023-07-13 02:18:35 +0330

    Commit changes and select last ten inserted records

--- report.py
+++ report.py
@@ -2,6 +2,9 @@ import psutil
 import sqlite3
 
 DB_NAME = "ram_usage"
+DEFAULT_LIMIT = 10
+
+
 db = sqlite3.connect(f"{DB_NAME}.db")
 cursor = db.cursor()
 
@@ -13,11 +16,17 @@ create_table = (
     'used integer not null, '
     'total integer not null);'
 )
-
-insert_query = f'insert into {DB_NAME} (free, available, used, total) values (?, ?, ?, ?)'
+insert_query = f'insert into {DB_NAME} (free, available, used, total) values (?, ?, ?, ?);'
+last_n_records = f'select * from {DB_NAME} limit {DEFAULT_LIMIT};'
 
 cursor.execute(create_table)
 
 ram = psutil.virtual_memory()
 
 cursor.execute(insert_query, (ram.free, ram.available, ram.used, ram.total))
+db.commit()
+
+data = cursor.execute(last_n_records).fetchall()
+
+cursor.close()
+print(data)
