cf0f271ea4beeb42bf157b67d4995baf2714b827
Amir Shabani <amir@shabani.dev>
2023-07-13 16:54:43 +0330

    Add basic authentication

--- api.py
+++ api.py
@@ -1,14 +1,48 @@
-from typing import List
-from fastapi import FastAPI
+import os
+from typing import List, Annotated
+from functools import lru_cache
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from report import ram_usage_instance
 from serializers import RamUsage
+from dotenv import load_dotenv
 
 
 app = FastAPI()
+security = HTTPBasic()
+
+
+@lru_cache()
+def get_settings():
+    load_dotenv()
+    return {
+        "username": os.getenv("USERNAME"),
+        "password": os.getenv("PASSWORD"),
+    }
+
+
+def authenticated(
+        credentials: Annotated[HTTPBasicCredentials, Depends(security)],
+        settings: dict = Depends(get_settings),
+):
+    username = settings["username"]
+    password = settings["password"]
+
+    if not (
+        username == credentials.username and
+        password == credentials.password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    return username
 
 
 @app.get("/ram_usage/", response_model=List[RamUsage])
-async def ram_usage():
+async def ram_usage(_username: str = Depends(authenticated)):
     stats = ram_usage_instance.get_ram_stats()
     result = [
         RamUsage.model_validate(
