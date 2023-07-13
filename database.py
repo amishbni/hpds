6a1e38a447ca9e6056c8b44f9cce65c3a96fd8a6
Amir Shabani <amir@shabani.dev>
2023-07-13 11:01:33 +0330

    Separate database management into its own class

---
+++ database.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+
+class DatabaseManager:
+    def __init__(self, database_name):
+        self.db = sqlite3.connect(f"{database_name}.db")
+        self.cursor = self.db.cursor()
+
+    def __del__(self):
+        self.db.close()
+
+    def query(self, query, params=None):
+        if params is None:
+            result = self.cursor.execute(query)
+        else:
+            result = self.cursor.execute(query, **params)
+        self.db.commit()
+        return result
+
+    def create_table(self, table_name, columns):
+        query = (
+            f"create table if not exists {table_name} "
+            f'({", ".join(columns)});'
+        )
+
+        self.query(query)
+
+    def insert(self, table_name, columns, values):
+        query = (
+            f"insert into {table_name} "
+            f'({", ".join(columns)}) '
+            f'values ({", ".join(map(str, values))});'
+        )
+
+        self.query(query)
+
+    def select(self, table_name, columns=None, limit=None):
+        columns = ", ".join(columns) if columns is not None else "*"
+        limit = f" limit {limit}" if limit is not None else ""
+        query = (
+            f"select {columns} from {table_name}"
+            f"{limit};"
+        )
+        return self.query(query).fetchall()
--- report.py
+++ report.py
@@ -1,32 +1,29 @@
 import psutil
-import sqlite3
+from database import DatabaseManager
+
 
 DB_NAME = "ram_usage"
 DEFAULT_LIMIT = 10
 
 
-db = sqlite3.connect(f"{DB_NAME}.db")
-cursor = db.cursor()
-
-create_table = (
-    f'create table if not exists {DB_NAME} '
-    '(id integer primary key autoincrement, '
-    'free integer not null, '
-    'available integer not null, '
-    'used integer not null, '
-    'total integer not null);'
+db = DatabaseManager(DB_NAME)
+db.create_table(
+    DB_NAME,
+    [
+        "id integer primary key autoincrement",
+        "used integer not null",
+        "free integer not null",
+        "total integer not null",
+    ]
 )
-insert_query = f'insert into {DB_NAME} (free, available, used, total) values (?, ?, ?, ?);'
-last_n_records = f'select * from {DB_NAME} limit {DEFAULT_LIMIT};'
-
-cursor.execute(create_table)
 
 ram = psutil.virtual_memory()
 
-cursor.execute(insert_query, (ram.free, ram.available, ram.used, ram.total))
-db.commit()
+db.insert(
+    DB_NAME,
+    ["used", "free", "total"],
+    [ram.used, ram.free, ram.total],
+)
 
-data = cursor.execute(last_n_records).fetchall()
-
-cursor.close()
-print(data)
+result = db.select(DB_NAME, columns=["used", "free", "total"], limit=DEFAULT_LIMIT)
+print(result)
