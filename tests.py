9763fd9c4ca5e9c365d154a3d5bfa9abdfa125fe
Amir Shabani <amir@shabani.dev>
2023-07-13 20:25:52 +0330

    Add test for reports

---
+++ tests.py
@@ -0,0 +1,9 @@
+from report import RamUsage
+
+
+def test_set_and_get_ram_stats():
+    ram_usage_instance = RamUsage("test")
+    ram_usage_instance.set_ram_stats()
+    results = ram_usage_instance.get_ram_stats()
+    assert len(results) == 1
+    ram_usage_instance.drop_table_if_exists()
